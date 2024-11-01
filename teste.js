6c39e8120ecc3e53f5f941275a3c77bfdb9236b3
Jhon-vlfs <joao.vlfreitas@aluno.educacao.pe.gov.br>
2024-11-01 19:34:43 -0300

    FEITO COM VINI

--- teste.js
+++ teste.js
@@ -1,13 +1,21 @@
-let num1, num2, minNum, maxNum, result, soma;
+//! Faça um programa que calcule o número médio de alunos por turma. Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma. As turmas não podem ter mais de 40 alunos.
 
-num1 = parseInt(prompt('Diga um número: '));
-num2 = parseInt(prompt('Diga outro número: '));
-minNum = Math.min(num1, num2);
-maxNum = Math.max(num1, num2);
-result = '';
-soma = 0;
-for(let i = minNum + 1; i < maxNum; i++){ 
-    result += i + " ";
-    soma += i;
+function mediaAlunosPorTurma(){
+
+    let turmas = Number(prompt("Quantas turmas existem?"));
+    let alunos = 0;
+    let media = 0;
+
+    for(let i = 0; i < turmas; i++){
+        alunos = Number(prompt(`Quantos alunos tem a turma ${i + 1}?`));
+            if(alunos > 40){
+                alert("A turma não pode ter mais de 40 alunos!");
+                i--;
+            }
+        media += alunos;            
+    }
+    media = media / turmas;
+    alert(`A media de alunos por turma é ${media.toFixed(2)}`);
 }
-document.write(`Os números inteiros correspondidos entre ${minNum} e ${maxNum} são: ${soma}`);
+
+mediaAlunosPorTurma();
